05f5aa41f6d736cfa602fb26d2ad4126405f42a3
Brooks <Skoorb412785@yahoo.com>
2020-03-10 10:21:15 -0400

    added more buttons

--- src/component/button/Button.stories.js
+++ src/component/button/Button.stories.js
@@ -9,3 +9,27 @@ label="Enter"
 white
 Button
 />)
+
+.add('Create Button', () => <Button
+label="Create"
+white
+Button
+/>)
+
+.add('Read Button', () => <Button
+label="Read"
+white
+Button
+/>)
+
+.add('Update Button', () => <Button
+label="Update"
+white
+Button
+/>)
+
+.add('Delete Button', () => <Button
+label="Delete"
+white
+Button
+/>)
