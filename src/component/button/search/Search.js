8265f759c8fcde66776e93a109d0205d512f89f6
Brooks <Skoorb412785@yahoo.com>
2020-03-10 11:06:38 -0400

    made basic search bar

---
+++ src/component/button/search/Search.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Search.css'
+import Form from '../form/Form'
+import Button from '../Button'
+
+const Search = (props) => {
+    let classList=''
+
+    if (props.grid) {
+        classList += ` grid`
+    }
+
+    return <div className={classList}>
+    <Form
+    placeholder="Search"
+    />
+    <Button
+    label="Search"
+    white
+    />
+    </div>
+}
+
+export default Search
