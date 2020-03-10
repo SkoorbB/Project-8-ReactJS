cce6cf3ccba949570bfd26028a1c6edb668cff26
Brooks <Skoorb412785@yahoo.com>
2020-03-10 15:42:58 -0400

    added button to jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap'
 import styled from 'styled-components';
 import homeImage from '../assets/homeImage.jpg';
+import Button from '../component/buttons/Button'
 
 const Styles = styled.div`
     .jumbotron {
@@ -31,7 +32,9 @@ export const Jumbotron = () => (
             <Container>
                 <h1>Welcome To TrendGetters</h1>
                 <p>Come in and see which shows and movies are trending</p>
-
+                <Button 
+                label="Enter"
+                white/>
             </Container>
         </Jumbo>
     </Styles>
