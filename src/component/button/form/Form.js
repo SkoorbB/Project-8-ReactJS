5af720706e3e986be25f4d2af633c884c853428e
Brooks <Skoorb412785@yahoo.com>
2020-03-10 09:18:14 -0400

    added basic form

---
+++ src/component/button/form/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Form.css';
+
+const Form = (props) => {
+    //let classList = ''
+
+return <div>
+    <div>    
+    <input /*className={classList}*/
+    placeholder={props.placeholder}>
+    </input>
+    </div>
+    </div>
+}
+
+
+export default Form;
