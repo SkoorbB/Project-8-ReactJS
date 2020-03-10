1e189c4ffee95c7aef3adbc32b38cd6af24d8810
Chakhan Smith <ccsmith995@gmail.com>
2020-03-10 14:02:33 -0400

    Just added template for homepage background

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap'
 import styled from 'styled-components';
+import homeImage from '../assets/homeImage.jpg';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+    .jumbotron {
+        background: url(${homeImage}) no-repeat fixed bottom;
+        background-size: cover;
+        height: 500px;
+        position: relative;
+        color: red;
+    }
+
+    .overlay {
+        opacity: 0.6;
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: -1;
+    }
+
+`;
 
 export const Jumbotron = () => (
     <Styles>
