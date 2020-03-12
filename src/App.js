68eafdc939d0863647da7106f61bc8137fc4969d
Chakhan Smith <ccsmith995@gmail.com>
2020-03-12 14:47:12 -0400

    Fixing merge conflicts...again

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-<<<<<<< HEAD
-        
         {/* <Layout> */}
-=======
-        <Layout>
->>>>>>> origin
           <Router>
             <Switch>
               <Route exact path="/" component={Home} />
