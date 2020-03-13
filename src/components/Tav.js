bd6531fd76df21da4f2a6f284aee39281ab3432a
Brian Brown II <bkeithbii@gmail.com>
2020-03-13 14:32:43 -0400

    Presentation rough draft(very rough, super rough)

--- src/components/Tav.js
+++ src/components/Tav.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
-import '../components/Tav.css';
-import  { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "../components/Tav.css";
+import { Link } from "react-router-dom";
 
 class Tav extends Component {
-    render() {
-        return (
-        <div>
+  render() {
+    return (
+      <div>
         <ul>
-        <Link to="/Shows">
-  <li><a class="active" href="#home">Home</a></li>
-  </Link>
-  <Link to="/">
-  <li><a href="#movies">Movies</a></li>
-  </Link>
-  <Link to="/Shows">
-  <li><a href="#shows">Shows</a></li>
-  </Link>
-  <li><a href="#library">Library</a></li>
-    </ul>
-    </div>
+          <Link to="/">
+            <li>
+              <a class="active" href="#home">
+                Home
+              </a>
+            </li>
+          </Link>
+          <Link to="/movies">
+            <li>
+              <a href="#movies">Movies</a>
+            </li>
+          </Link>
+          <Link to="/shows">
+            <li>
+              <a href="#shows">Shows</a>
+            </li>
+          </Link>
+          <li>
+            <a href="#library">Library</a>
+          </li>
+        </ul>
+      </div>
     );
+  }
 }
-}
-
 
 export default Tav;
